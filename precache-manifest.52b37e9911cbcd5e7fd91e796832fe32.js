e4721d36dd66fd8ee98f90803603add264ec9f65
Israr Khan <iisraromar@gmail.com>
2020-07-04 23:32:34 +0500

    Updates

---
+++ precache-manifest.52b37e9911cbcd5e7fd91e796832fe32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d7987246fe3fae35e599a10fe0df6ac",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "239f546ea1a20e2447fb",
+    "url": "/myapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1d82f6806564a41ca99b",
+    "url": "/myapp/static/js/2.e1e040c7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myapp/static/js/2.e1e040c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "239f546ea1a20e2447fb",
+    "url": "/myapp/static/js/main.2a40ce68.chunk.js"
+  },
+  {
+    "revision": "74fe74c9f553778d370e",
+    "url": "/myapp/static/js/runtime-main.ad75bf6b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
